56c2acdf81690900f8a9b5c67d11dc5de1814e93
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-24 15:50:08 +0300

    Queue Mesage oto delete

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -2,19 +2,24 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
+//Bağlantı oluşturma
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cloudamqp.com/wjagymjh");
 
+//Bağlantı etkinleştirme ve kanal oluşturma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
+//kuyruk oluşturma
 channel.QueueDeclare(queue: "Example-queue", exclusive: false);
 
+//Kuyruktan mesaj okuma
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "Example-queue", false, consumer);
+channel.BasicConsume(queue: "Example-queue", autoAck: false, consumer);
 consumer.Received += (sender,e) =>
 {
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
 
+//Bağlantı oluşturma
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cloudamqp.com/wjagymjh");
 
+//Bağlantı etkinleştirme ve kanal oluşturma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
+//kuyruk oluşturma
 channel.QueueDeclare(queue:"Example-queue", exclusive: false);
 
-//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-//channel.BasicPublish(exchange: "", routingKey:"Example-queue", body: message);
-
+//Kuyruktan mesaj gönderme
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
