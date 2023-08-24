741cb71635d5b09acff8af680b7a2704eb60d7d9
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-24 15:19:29 +0300

    Basic RabbitMQ

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cloudamqp.com/wjagymjh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "Example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "Example-queue", false, consumer);
+consumer.Received += (sender,e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cloudamqp.com/wjagymjh");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue:"Example-queue", exclusive: false);
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey:"Example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "Example-queue", body: message);
+}
+Console.Read();
+
