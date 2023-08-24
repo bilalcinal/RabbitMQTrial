3c41760e5ad42229c3e391eddfd252c4c5723086
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-24 16:34:07 +0300

    Direct Exchange

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -10,17 +10,35 @@ factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//kuyruk oluşturma
-channel.QueueDeclare(queue: "Example-queue", exclusive: false, durable:true);
+//Exchange tanımlama
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type:ExchangeType.Direct);
 
-//Kuyruktan mesaj okuma
+//Yolanan değerleri tüketir
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "Example-queue", autoAck: false, consumer);
-channel.BasicQos(0, 1, false);
-consumer.Received += (sender,e) =>
+channel.BasicConsume(queue: queueName, autoAck: true, consumer:consumer);
+consumer.Received += (sender, e) =>
 {
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
 };
-
 Console.Read();
+
+////// Basic RabbitMQ //////
+////kuyruk oluşturma
+//channel.QueueDeclare(queue: "Example-queue", exclusive: false, durable: true);
+
+////Kuyruktan mesaj okuma
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: "Example-queue", autoAck: false, consumer);
+//channel.BasicQos(0, 1, false);
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+//};
+
+//Console.Read();
+//////////////////////////
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -9,17 +9,35 @@ factory.Uri = new("amqps://wjagymjh:CXETsmwe1_FRjhexW3DnApC73t3sxI4B@toad.rmq.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//kuyruk oluşturma
-channel.QueueDeclare(queue:"Example-queue", exclusive: false, durable:true);
+//Exchange Tanımlama
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
 
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
-//Kuyruktan mesaj gönderme
-for (int i = 0; i < 100; i++)
+//Yollanan değer
+while (true)
 {
-    await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
-    channel.BasicPublish(exchange: "", routingKey: "Example-queue", body: message , basicProperties : properties);
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey: "direct-queue-example", body: byteMessage);
 }
 Console.Read();
 
+
+
+
+////// Basic RabbitMQ ////////
+////kuyruk oluşturma
+//channel.QueueDeclare(queue:"Example-queue", exclusive: false, durable:true);
+
+//IBasicProperties properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+////Kuyruktan mesaj gönderme
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(200);
+//    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+//    channel.BasicPublish(exchange: "", routingKey: "Example-queue", body: message , basicProperties : properties);
+//}
+//Console.Read();
+/////////////////////
